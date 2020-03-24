100ccf7c31efb996b49fa91ef92a8640a9f9e382
Wesley Jacobs <w.jacobs.632@gmail.com>
2020-03-24 09:45:28 -0700

    added styling to the signup

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,21 +1,39 @@
-import React from 'react';
-import { View, StyleSheet, Text, Button } from 'react-native';
-
-
+import React from "react";
+import { View, StyleSheet } from "react-native";
+import { Text, Button, Input } from "react-native-elements";
+import Spacer from "../components/Spacer";
 
 const SignupScreen = ({ navigation }) => {
   return (
-    <>
-      <Text style={{fontSize: 30}}>SignupScreen</Text>
-      <Button title='Go to Signin' onPress={() => navigation.navigate('Signin')}/>
-      <Button title='Go to MainFlow' onPress={() => navigation.navigate('mainFlow')}/>
-
-    </>
-  )
+    <View style={styles.container}>
+      <Spacer>
+        <Text h3>Sign up for Tracker</Text>
+      </Spacer>
+      <Spacer>
+        <Input label="Email" />
+      </Spacer>
+      <Spacer>
+        <Input label="Password" />
+      </Spacer>
+      <Spacer>
+        <Button title="Sign Up" onPress={() => navigation.navigate("Signin")} />
+      </Spacer>
+    </View>
+  );
 };
+// You don't have to use a function. Use function if you intend to use the navigation prop for navigating
+SignupScreen.navigationOptions = () => {
+  return {
+    headerShown: false,
+  }
+}
 
 const styles = StyleSheet.create({
-  
+  container:{
+    flex: 1,
+    justifyContent: 'center',
+    marginBottom: 350,
+  }
 });
 
 export default SignupScreen;
