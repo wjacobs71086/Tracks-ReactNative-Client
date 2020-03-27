16492cbf7908dda470da69553a94641819a6d886
Wesley Jacobs <w.jacobs.632@gmail.com>
2020-03-27 10:24:07 -0700

    Added currentLocation tracking on map

--- src/screens/TrackCreateScreen.js
+++ src/screens/TrackCreateScreen.js
@@ -1,24 +1,30 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useContext } from 'react';
 import { View, StyleSheet } from 'react-native';
 import {Text } from 'react-native-elements';
 import { SafeAreaView } from 'react-navigation'
 import Map from '../components/Map';
 import { requestPermissionsAsync, watchPositionAsync, Accuracy } from 'expo-location';
+import { Context as LocationContext } from '../context/LocationContext';
+
 //import '../_mockLocation';
 
 
 const TrackCreateScreen = () => {
   const [ err, setErr ] = useState(null);
+  const { addLocation } = useContext(LocationContext);
 
-  const startWatching = async() =>{
+  const startWatching = async () => {
     try{
       await requestPermissionsAsync();
-      await watchPositionAsync({
+      await watchPositionAsync(
+        {
         accuracy: Accuracy.BestForNavigation,
         timeInterval: 1000,
         distanceInterval: 10
-      }, (location) => {
-        console.log(location)
+        }, 
+        location => {
+        //This is the "callback" for using the location?
+        addLocation(location);
       })
     } catch(err) {
       setErr(err)
